39d67e1a62b946cc3cdf5b49a92d07ff111e001d
gaisuke <danimunif@cs.unej.ac.id>
2023-06-22 07:03:52 +0700

    add utils pagination

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
 
 func RandString(length int) string {
 	var letterRune = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
@@ -12,4 +16,27 @@ func RandString(length int) string {
 	}
 
 	return string(b)
+}
+
+func Paginate(offset, limit int) func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		page := offset
+
+		// jika page lebih kecil dari atau sama dengan 0, maka set page ke 1
+		if page <= 0 {
+			page = 1
+		}
+
+		pageSize := limit
+
+		switch {
+		case pageSize > 100:
+			pageSize = 100
+		case pageSize <= 0:
+			pageSize = 10
+		}
+
+		offset = (page - 1) * pageSize
+		return db.Offset(offset).Limit(pageSize)
+	}
 }
